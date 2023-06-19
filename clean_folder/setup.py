30e90c5205e3004f748577bf16a0f8fb599b2e91
EfimenkoAlexey <alexeyefimenko1@gmail.com>
2023-06-19 16:00:25 +0300

    update

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name='clean_folder',
@@ -6,7 +6,7 @@ setup(
     description='Code small program',
     url='https://github.com/EfimenkoAlexey/homework_06/tree/main/clean_folder',
     author='Alexey Efimenko',
-    packages=find_namespace_packages(),
-    install_requires=["sys", "os", "shutil"],
-    entry_points={'console_scripts': ['clean_folder = clean_folder.clean_folder:main']}
+    packages=find_packages(),
+    # install_requires=["sys", "os", "shutil"],
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean_folder:main']}
 )
